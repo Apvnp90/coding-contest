b0b54a544006acfab02a540f1d65de7af027e119
Annamalai, Prabhakar <prabhakar.annamalai@anywhere.re>
2026-03-31 12:20:23 -0400

    Add WebRequestTrackingFilter to capture incoming HTTP requests

--- code-challenge-api/src/main/java/contest/code_challenge/config/ApplicationInsightsConfig.java
+++ code-challenge-api/src/main/java/contest/code_challenge/config/ApplicationInsightsConfig.java
@@ -1,7 +1,9 @@
 package contest.code_challenge.config;
 
 import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +22,17 @@ public class ApplicationInsightsConfig {
         }
         
         return telemetryClient;
+    }
+
+    @Bean
+    public FilterRegistrationBean<WebRequestTrackingFilter> webRequestTrackingFilter() {
+        FilterRegistrationBean<WebRequestTrackingFilter> registrationBean = new FilterRegistrationBean<>();
+        
+        WebRequestTrackingFilter filter = new WebRequestTrackingFilter();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        
+        return registrationBean;
     }
 }
