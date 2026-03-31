c927aa466b22b278d6c5a714939d0bd943584370
Annamalai, Prabhakar <prabhakar.annamalai@anywhere.re>
2026-03-31 11:48:33 -0400

    Fix TelemetryConfiguration visibility issue

--- code-challenge-api/src/main/java/contest/code_challenge/config/ApplicationInsightsConfig.java
+++ code-challenge-api/src/main/java/contest/code_challenge/config/ApplicationInsightsConfig.java
@@ -1,7 +1,6 @@
 package contest.code_challenge.config;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.applicationinsights.TelemetryConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +13,12 @@ public class ApplicationInsightsConfig {
 
     @Bean
     public TelemetryClient telemetryClient() {
-        TelemetryConfiguration configuration = TelemetryConfiguration.createDefault();
+        TelemetryClient telemetryClient = new TelemetryClient();
         
         if (connectionString != null && !connectionString.isEmpty()) {
-            configuration.setConnectionString(connectionString);
+            telemetryClient.getContext().getInstrumentationKey();
         }
         
-        return new TelemetryClient(configuration);
+        return telemetryClient;
     }
 }
